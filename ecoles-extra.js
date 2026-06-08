209134a1f380a582d0fb77e651ee338533623cca
titouanbed <bedostitouan0@gmail.com>
2026-06-08 13:26:08 +0200

    Add files via upload

---
+++ ecoles-extra.js
@@ -0,0 +1,71 @@
+window.ECOLES_EXTRA = [
+  {
+    "uai": "0731045P",
+    "formations": [
+      {
+        "intitule": "CAP PEINTURE APPLICATEUR DE REVËTEMENT",
+        "niveau": "CAP (Bac-2)",
+        "rythme": null
+      },
+      {
+        "intitule": "CAP EQUIPIER POLYVALENT DE COMMERCE",
+        "niveau": "CAP (Bac-2)",
+        "rythme": null
+      },
+      {
+        "intitule": "CAP MENUISIER FABRICANT",
+        "niveau": "CAP (Bac-2)",
+        "rythme": null
+      }
+    ],
+    "effectif_promo": 10.0,
+    "aide_entreprise": "oui_partiel",
+    "frais_scolarite": "gratuit",
+    "email_admissions": "ce.0731045p@ac-grenoble.fr",
+    "telephone": "04 79 32 40 17",
+    "website": "https://erea-mirantin.ent.auvergnerhonealpes.fr/"
+  },
+  {
+    "uai": "0383537N",
+    "formations": [
+      {
+        "intitule": "- Titre Pro Technicien Traitement des Eaux",
+        "niveau": "Bac Pro",
+        "rythme": "3 semaines en entreprises/ 2 semaines en centre"
+      },
+      {
+        "intitule": "- BTSA Gestion et Maitrise de l'Eau",
+        "niveau": "BTS / BUT (Bac+2)",
+        "rythme": "4 semaines en entreprises/ 4 semaines en centre"
+      },
+      {
+        "intitule": "- Licence Pro Nature en Ville",
+        "niveau": "Licence Pro (Bac+3)",
+        "rythme": "4 semaines en entreprises/ 4 semaines en centre"
+      },
+      {
+        "intitule": "- Licence Pro Milieux Aquatiques et Eaux Pluviales",
+        "niveau": "Licence Pro (Bac+3)",
+        "rythme": "4 semaines en entreprises/ 4 semaines en centre"
+      },
+      {
+        "intitule": "- Licence Pro TP DTRH",
+        "niveau": "Licence Pro (Bac+3)",
+        "rythme": "4 semaines en entreprises/ 4 semaines en centre"
+      },
+      {
+        "intitule": "- Licence Pro Pompage",
+        "niveau": "Licence Pro (Bac+3)",
+        "rythme": "4 semaines en entreprises/ 4 semaines en centre"
+      }
+    ],
+    "nb_partenaires": 100.0,
+    "effectif_promo": 20.0,
+    "aide_entreprise": "oui_actif",
+    "delai_placement": "1_2mois",
+    "frais_scolarite": "gratuit",
+    "email_admissions": "mathilde.juston@educagri.fr",
+    "telephone": "647007249",
+    "website": "https://vienne.educagri.fr/"
+  }
+];
